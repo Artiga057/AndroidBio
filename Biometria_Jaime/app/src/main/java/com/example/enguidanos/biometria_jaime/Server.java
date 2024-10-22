b805042c7d0ff3c0a7dbd953a649f226b04e3d5f
jaineed01 <126506425+jaineed01@users.noreply.github.com>
2024-10-22 11:58:31 +0200

    App Sprint0

---
+++ Biometria_Jaime/app/src/main/java/com/example/enguidanos/biometria_jaime/Server.java
@@ -0,0 +1,47 @@
+package com.example.enguidanos.biometria_jaime;
+
+import android.util.Log;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.RequestQueue;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Server {
+    private static final String URL = "http://172.20.10.14/prueba/guardarMedicion.php?";
+    public static void guardarMedicion(final String ozono, final String temperatura, RequestQueue requestQueue) {
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.POST,    // Método HTTP (POST).
+                URL,                // URL del servidor.
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Errores.", error.toString());
+                    }
+                }
+
+        ) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                // Se definen los parámetros a enviar en la solicitud (nivel de CO2 y temperatura).
+                params.put("ozono", ozono);
+                params.put("temperatura", temperatura);
+                return params;
+
+            }
+        };
+        // Se agrega la solicitud a la cola de solicitudes para su procesamiento.
+        requestQueue.add(stringRequest);
+    }
+}
